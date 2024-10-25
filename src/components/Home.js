19b3d94c8a33d3918a5ce70fb5c44a76fc105227
Shashank Gavale <shashankmgavale2004@gmail.com>
2024-10-25 21:07:52 +0530

    Second

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import Navbar from './Navbar';
-import Counter from './Counter';
-import github from '../Images/social.png';
-import linkedin from '../Images/linkedin.png';
-import twitter from '../Images/twitter.png';
-import whatsapp from '../Images/whatsapp.png';
+import React from "react";
+import Navbar from "./Navbar";
+import Counter from "./Counter";
+import github from "../Images/social.png";
+import linkedin from "../Images/linkedin.png";
+import twitter from "../Images/twitter.png";
+import whatsapp from "../Images/whatsapp.png";
 // import man from '../Images/man.png'; // Import your profile image
 // import './Home.css'; // Import your CSS file
 
@@ -14,12 +14,14 @@ export default function Home() {
       <Navbar />
       <div className="relative p-12">
         <div className="absolute left-80 top-32 text-lg space-y-5">
-          <p className='text-2xl'>Software Developer</p>
+          <p className="text-2xl">Software Developer</p>
           <div className="flex items-center">
             <p className="text-6xl">Hello, I'm</p>
           </div>
-          <p className="text-7xl text-green-500 animate-typing">Shashank Gavale</p>
-          
+          <p className="text-7xl text-green-500 animate-typing">
+            Shashank Gavale
+          </p>
+
           <div className="space-y-1 text-2xl mt-20">
             <p>I excel in crafting elegant digital experiences,</p>
             <p>I am proficient in various programming languages and</p>
@@ -28,15 +30,31 @@ export default function Home() {
 
           <div className="flex items-center space-x-5 mt-8">
             <button className="px-4 py-2 bg-green-500 text-white rounded-full hover:bg-green-600 transition duration-300">
-              Download CV
+              <a href="/CV.pdf" download>
+                Download CV
+              </a>
             </button>
-            
-            <img src={github} alt="GitHub" className="w-8 h-8" />
-            <img src={linkedin} alt="LinkedIn" className="w-8 h-8" />
-            <img src={twitter} alt="Twitter" className="w-8 h-8" />
-            <img src={whatsapp} alt="WhatsApp" className="w-8 h-8" />
+
+            <a href="https://github.com/Shankzzz1">
+              {" "}
+              <img src={github} alt="GitHub" className="w-8 h-8" />
+            </a>
+            <a href="https://linkedin.com/in/shashank-gavale-041148261">
+              <img src={linkedin} alt="LinkedIn" className="w-8 h-8" />
+            </a>
+            <a href="https://x.com/1_Shankzzz">
+              {" "}
+              <img src={twitter} alt="Twitter" className="w-8 h-8" />
+            </a>
+            <a
+              href="https://wa.me/9834431768" // replace with the actual phone number
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={whatsapp} alt="WhatsApp" className="w-8 h-8" />
+            </a>
           </div>
-          <div className='relative top-20'>
+          <div className="relative top-20">
             <Counter />
           </div>
         </div>
